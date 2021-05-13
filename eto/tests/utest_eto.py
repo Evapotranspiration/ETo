ae37988edc9e86f86a382302fe1d8b63f1a1d517
mullenkamp <mullenkamp1@gmail.com>
2021-05-13 16:13:23 +1200

    utests

--- eto/tests/utest_eto.py
+++ eto/tests/utest_eto.py
@@ -38,32 +38,6 @@ eto1 = et1.eto_fao()
 
 
 
-def test_eto_fao_daily():
-    et1 = ETo(tsdata, 'H', z_msl, lat, lon, TZ_lon)
-    eto1 = et1.eto_fao()
-    res1 = tsresults['ETo_FAO_mm'].sum()
-
-    assert eto1 == res1
-
-et1 = ETo(tsdata, 'D', z_msl, lat, lon, TZ_lon)
-
-def test_eto_har_daily():
-    eto2 = et1.eto_hargreaves().sum()
-    res1 = tsresults['ETo_Har_mm'].sum()
-
-    assert eto2 == res1
-
-def test_eto_fao_hourly():
-    tsdata2 = et1.ts_param[['R_s', 'T_mean', 'e_a']]
-    tsdata3 = et1.tsreg(tsdata2, 'H', 'time')
-    et2 = ETo(tsdata3, 'H', z_msl, lat, lon, TZ_lon)
-    eto3 = et2.eto_fao().sum()
-
-    res1 = tsresults['ETo_FAO_mm'].sum()
-
-    assert eto3 > res1
-
-
 
 
 
